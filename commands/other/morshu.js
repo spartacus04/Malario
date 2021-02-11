03bed395a700eb11b237f9e9e046be3f88cd2355
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-02-11 21:50:52 +0100

    cambio il link di m.morshu

--- commands/other/morshu.js
+++ commands/other/morshu.js
@@ -14,7 +14,7 @@ module.exports = class RandomNumberCommand extends Command {
   run(message) {
     message.channel.send({
         files: [{
-            attachment: "https://www.dropbox.com/s/lialpiertk1tlp3/Morshu-Beatbox.gif?dl=1",
+            attachment: "https://www.dropbox.com/s/j7nbdx5pzn1u75c/Morshu-Beatbox.gif?dl=1",
             name: 'morshu.gif'
         }]
     })
